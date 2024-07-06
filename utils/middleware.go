45656c2cce701118f8e9ce0d713cf3119f8f8a8f
QuodEstDubitandum <d.steblin.dev@gmail.com>
2024-07-07 00:52:17 +0200

    change logging to log crawler requests

--- utils/middleware.go
+++ utils/middleware.go
@@ -12,7 +12,7 @@ func AuthMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		requestKey := c.Get("x-api-key")
 		if requestKey != apiKey {
-			log.Printf("Expected apiKey %s, got %s", apiKey, requestKey)
+			log.Printf("Unauthorized access from: %s", c.IP())
 			return c.Status(403).Send([]byte("Unauthorized"))
 		}
 		return c.Next()
